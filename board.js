e75057f81a19ab49d1893482748b380352d3bb93
Andres Canas-Baratta <andres.canas@yahoo.com>
2025-06-26 22:28:34 -0700

    Initiated battleship project: created greeting, user board size election

---
+++ board.js
@@ -0,0 +1,8 @@
+main();
+
+console.table(board);
+console.table({
+  A: ["-", "🟠", "-"],
+  B: ["-", "-", "❗"],
+  C: ["-", "-", "-"],
+}); // go ahead and try copying and pasting this into a script and testing it out for yourself <3
